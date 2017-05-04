5c4a29fb6bbdbd60a0802e248657bcd63e5efdaa
Benjamin Chylla <ejbgames@gmail.com>
2017-05-04 16:22:55 -0500

    Second last commit.

--- processStudent.cpp
+++ processStudent.cpp
@@ -19,12 +19,10 @@
 #include <iterator>
 #include <algorithm>
 
-using namespace std;
-
 void fillStudents(std::istream &inFile,
 		std::vector<GradStudent> &gstudents,
 		std::vector<UndergradStudent> &ugstudents) {
-	string line;
+	std::string line;
 	while(getline(inFile, line)) {
 		std::vector<std::string> inputs;
 		for (int i = 0; i < 11; i++) {
@@ -37,19 +35,19 @@ void fillStudents(std::istream &inFile,
 				inputs.push_back(line);
 			}
 		}
-		string studentType = inputs[0];
-		string name = inputs[1];
-		double score1 = stod(inputs[2]);
-		double score2 = stod(inputs[3]);
-		double score3 = stod(inputs[4]);
-		double score4 = stod(inputs[5]);
-		double score5 = stod(inputs[6]);
-		double score6 = stod(inputs[7]);
-		double projectScore = stod(inputs[8]);
-		string resHallOrResearchArea = inputs[9];
-		string yearInSchoolOrAdvisor = inputs[10];
+		std::string studentType = inputs[0];
+		std::string name = inputs[1];
+		double score1 = std::stod(inputs[2]);
+		double score2 = std::stod(inputs[3]);
+		double score3 = std::stod(inputs[4]);
+		double score4 = std::stod(inputs[5]);
+		double score5 = std::stod(inputs[6]);
+		double score6 = std::stod(inputs[7]);
+		double projectScore = std::stod(inputs[8]);
+		std::string resHallOrResearchArea = inputs[9];
+		std::string yearInSchoolOrAdvisor = inputs[10];
 
-		vector<double> assignmentScore = {score1, score2, score3, score4, score5, score6};
+		std::vector<double> assignmentScore = {score1, score2, score3, score4, score5, score6};
 		if (studentType.compare("U") == 0) {
 			//Student* aUGStudent = new UndergradStudent(name, assignmentScore, projectScore, resHallOrResearchArea, yearInSchoolOrAdvisor);
 			ugstudents.push_back(UndergradStudent(name, assignmentScore, projectScore, resHallOrResearchArea, yearInSchoolOrAdvisor));
@@ -67,19 +65,19 @@ void printStudents(const std::vector<std::reference_wrapper<Student>> &students)
 		return a.getTotal() > b.getTotal();
 			});
 	for (int i = 0; i < students.size(); i++) {
-		Student newStudent = students[i].get();
-		newStudents.printDetails();
+		students[i].Student::printDetails();
+		//newStudent.printDetails();
 	}
 }
 
 void computeStatistics(std::vector<std::reference_wrapper<Student>> &students) {
-	cout << "Number of students = " << students.size() << endl;
+	std::cout << "Number of students = " << students.size() << std::endl;
 	double totalScore;
 	for (int i = 0; i < students.size(); i++) {
 		totalScore += students[i].getTotal();
 	}
-	cout << "The mean of the total score = " << totalScore/students.size() << endl;
-	cout << "The sorted list of students (id, name, total, grade) in descending order: " << endl;
+	std::cout << "The mean of the total score = " << totalScore/students.size() << std::endl;
+	std::cout << "The sorted list of students (id, name, total, grade) in descending order: " << std::endl;
 	struct less_than_key
 	{
 		inline bool operator() (Student student1, Student student2)
@@ -87,8 +85,8 @@ void computeStatistics(std::vector<std::reference_wrapper<Student>> &students) {
 			return (student1.getTotal() < student2.getTotal());
 		}
 	};
-	sort(students.begin(), students.end(), less_than_key());
+	std::sort(students.begin(), students.end(), less_than_key());
 	for (int i = 0; i < students.size(); i++) {
-		cout << students[i].getId() << ", " << students[i].getName() << ", " << students[i].getTotal() << ", " << students[i].getGrade() << endl;
+		std::cout << students[i].getId() << ", " << students[i].getName() << ", " << students[i].getTotal() << ", " << students[i].getGrade() << std::endl;
 	}
 }
